492dc57e7a92942ee03dc5295a652631d3678a1e
jpPinheiro03 <joao.pinheiro@sptech.school>
2025-06-17 13:15:45 -0300

    id codigo

--- src/controllers/galeriaController.js
+++ src/controllers/galeriaController.js
@@ -1,43 +1,11 @@
-var GaleriaModel = require("../models/GaleriaModel");
-
-function buscarPorCnpj(req, res) {
-  var cnpj = req.query.cnpj;
-
-  GaleriaModel.buscarPorCnpj(cnpj).then((resultado) => {
-    res.status(200).json(resultado);
-  });
-}
+var galeriaModel = require("../models/galeriaModel");
 
 function listar(req, res) {
-  GaleriaModel.listar().then((resultado) => {
+  galeriaModel.listar().then((resultado) => {
     res.status(200).json(resultado);
   });
 }
 
-function buscarPorId(req, res) {
-  var id = req.params.id;
-
-  GaleriaModel.buscarPorId(id).then((resultado) => {
-    res.status(200).json(resultado);
-  });
-}
-
-function cadastrar(req, res) {
-  var cnpj = req.body.cnpj;
-  var razaoSocial = req.body.razaoSocial;
-
-  GaleriaModel.buscarPorCnpj(cnpj).then((resultado) => {
-    if (resultado.length > 0) {
-      res
-        .status(401)
-        .json({ mensagem: `a Galeria com o cnpj ${cnpj} já existe` });
-    } else {
-      GaleriaModel.cadastrar(razaoSocial, cnpj).then((resultado) => {
-        res.status(201).json(resultado);
-      });
-    }
-  });
-}
 
 module.exports = {
   listar,
